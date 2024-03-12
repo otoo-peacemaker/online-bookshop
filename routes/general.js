e0ffe703769e6988771a226e3322afd80329bb5d
otoo-peacemaker <nanakwesiotoo@gmail.com>
2024-03-12 22:44:01 +0000

    path

--- routes/general.js
+++ routes/general.js
@@ -30,7 +30,7 @@ public_users.post("/register", (req,res) => {
 
 
 // Get the book list available in the shop
-public_users.get('/',async function (req, res) {
+public_users.get('/books',async function (req, res) {
 
   try {
     const allBooks = await books;
