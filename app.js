0c2a11113e5a1ebc8e380891d32642faee53027d
otoo-peacemaker <nanakwesiotoo@gmail.com>
2024-03-12 23:27:18 +0000

    jwt and session store

--- app.js
+++ app.js
@@ -76,10 +76,10 @@ app.use("/customer/auth/*", function auth(req,res,next){
   }
 });
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/customer", customer_routes);
-app.use("/books", genl_routes);
+app.use("/", genl_routes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
